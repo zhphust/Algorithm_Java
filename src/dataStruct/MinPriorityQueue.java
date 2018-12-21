b29ef016407046f8fc9502c539fdde4ad0f11d36
zhphust <zhphust09@163.com>
2018-12-21 21:01:35 +0800

    最小堆

--- src/dataStruct/MinPriorityQueue.java
+++ src/dataStruct/MinPriorityQueue.java
@@ -5,30 +5,30 @@ import java.util.Arrays;
 import java.util.Random;
 
 /**
- * 最小堆
+ * 由最小堆实现的最小优先队列
  * @author Alisa.Wang zhphust09@hust.edu.cn at 2018/11/24
  * @version 8.0
  */
-public class MinHeap<T extends Comparable<T>> {
-    private T[] array;
-    private int heapSize = 0;
+public class MinPriorityQueue<T extends Comparable<T>> {
+    private T[] heap;
+    private int size;
 
     @SuppressWarnings("unchecked")
-    public MinHeap() {
-        array = (T[])new Comparable[1];
-        array[0] = null;
+    public MinPriorityQueue() {
+        this(0);
     }
     @SuppressWarnings("unchecked")
-    public MinHeap(int max) {
-        array = (T[])new Comparable[max+1];
-        array[0] = null;
+    public MinPriorityQueue(int max) {
+        heap = (T[])new Comparable[max+1];
+        heap[0] = null;
+        size = 0;
     }
     @SuppressWarnings("unchecked")
-    public MinHeap(T[] a) {
-        array = (T[])new Comparable[a.length+1];
-        array[0] = null;                                    // 堆的索引为0的位不使用
+    public MinPriorityQueue(T[] a) {
+        heap = (T[])new Comparable[a.length+1];
+        heap[0] = null;                                    // 堆的索引为0的位不使用
         for (int i = 0; i < a.length; i++) {
-            array[i+1] = a[i];
+            heap[i+1] = a[i];
         }
         buildHeap();                                         // 建堆
     }
@@ -36,11 +36,11 @@ public class MinHeap<T extends Comparable<T>> {
     private void buildHeap() {
         /*
         * 在一个初始数组中，数组后一半默认已经满足堆的性质，
-        * 因此只需按逆序，从索引为 heapSize/2 的元素开始，
+        * 因此只需按逆序，从索引为 size/2 的元素开始，
         * 对每个元素逐级下沉，最终构建出整个堆。
         */
-        heapSize = array.length - 1;
-        for (int i = heapSize /2; i >= 1; i--) {
+        size = heap.length - 1;
+        for (int i = size /2; i >= 1; i--) {
             // 维护堆的性质，下沉
             sink(i);
         }
@@ -54,12 +54,12 @@ public class MinHeap<T extends Comparable<T>> {
         int right = 2 * i + 1;
         int min = i;
         // 孩子节点一定要和堆的长度而不是数组长度比较
-        if (left <= heapSize && array[left].compareTo(array[min]) < 0)
+        if (left <= size && heap[left].compareTo(heap[min]) < 0)
             min = left;
-        if (right <= heapSize && array[right].compareTo(array[min]) < 0)
+        if (right <= size && heap[right].compareTo(heap[min]) < 0)
             min = right;
         if (min != i) {
-            Swap.swap(array, min, i);
+            Swap.swap(heap, min, i);
             sink(min);                       //  递归，下沉以维护堆的性质
         }
     }
@@ -70,19 +70,20 @@ public class MinHeap<T extends Comparable<T>> {
      */
     @SuppressWarnings("unchecked")
     public void insert(T element) {
-        heapSize += 1;
-        if (heapSize == array.length) {
-            T[] temp = (T[]) new Comparable[heapSize*2];
-            System.arraycopy(array, 0, temp, 0, heapSize);
-            array = temp;
+        size += 1;
+        if (size == heap.length) {
+            T[] temp = (T[]) new Comparable[size *2];
+            System.arraycopy(heap, 0, temp, 0, size);
+            heap = temp;
         }
-        array[heapSize] = element;
-        swim(heapSize);
+        heap[size] = element;
+        swim(size);                 // 上浮
     }
 
-    private void swim(int i) {      // 上浮
-        while (i > 1 && array[i/2].compareTo(array[i]) > 0) {
-            Swap.swap(array, i, i/2);
+    // 上浮
+    private void swim(int i) {
+        while (i > 1 && heap[i/2].compareTo(heap[i]) > 0) {
+            Swap.swap(heap, i, i/2);
             i /= 2;
         }
     }
@@ -92,7 +93,7 @@ public class MinHeap<T extends Comparable<T>> {
      * @return 获得关键字最小元素
      */
     public T min() {
-        return array[1];
+        return heap[1];
     }
 
     /**
@@ -105,16 +106,16 @@ public class MinHeap<T extends Comparable<T>> {
             throw new NullPointerException("Heap is null.");
         }
 
-        T min = array[1];
-        array[1] = array[heapSize];
-        array[heapSize] = null;
-        heapSize--;
+        T min = heap[1];
+        heap[1] = heap[size];
+        heap[size] = null;
+        size--;
         sink(1);                           // 下沉
 
-        if (heapSize == array.length / 4) {
-            T[] temp = (T[])new Comparable[array.length / 4 + 1];
-            System.arraycopy(array, 0, temp, 0, heapSize+1);
-            array = temp;
+        if (size == heap.length / 4) {       // 调整堆的大小
+            T[] temp = (T[])new Comparable[heap.length / 4 + 1];
+            System.arraycopy(heap, 0, temp, 0, size +1);
+            heap = temp;
         }
 
         return min;
@@ -125,34 +126,32 @@ public class MinHeap<T extends Comparable<T>> {
      * @return 布尔值
      */
     public boolean isEmpty() {
-        return heapSize == 0;
+        return size == 0;
     }
 
     @SuppressWarnings("unchecked")
     public T delete(int i) {
-        T element = array[i];
-        array[i] = array[heapSize];
-        array[heapSize] = null;
-        heapSize--;
-        if (element == array[i])                             // 该堆末尾元素和待删除元素相等
-            return element;
-        if (array[i].compareTo(element) > 0) {                              // 新占位的元素变小，下沉
+        T element = heap[i];
+        heap[i] = heap[size];
+        heap[size] = null;
+        size--;
+        if (heap[i].compareTo(element) >= 0) {                              // 新占位的元素变小，下沉
             sink(i);
         } else {                                                            // 上浮
             swim(i);
         }
 
-        if (heapSize == array.length / 4) {
-            T[] temp = (T[])new Comparable[array.length / 4 + 1];
-            System.arraycopy(array, 0, temp, 0, heapSize+1);
-            array = temp;
+        if (size == heap.length / 4) {                       // 调整堆的大小
+            T[] temp = (T[])new Comparable[heap.length / 4 + 1];
+            System.arraycopy(heap, 0, temp, 0, size +1);
+            heap = temp;
         }
 
         return element;
     }
 
     public int size() {
-        return heapSize;
+        return size;
     }
 
     /**
@@ -160,41 +159,39 @@ public class MinHeap<T extends Comparable<T>> {
      */
     @SuppressWarnings("unchecked")
     public T[] heapSort() {
-        T[] list = (T[]) new Comparable[heapSize];
+        T[] list = (T[]) new Comparable[size];
         int i = 0;
-        while (heapSize != 1) {
-            list[i++] = array[1];
+        while (size != 1) {
+            list[i++] = heap[1];
             // 将堆中的根元素和堆中的最后一个元素交换
-            Swap.swap(array, 1, heapSize);
-            array[heapSize] = null;
-            heapSize--;
+            Swap.swap(heap, 1, size);
+            heap[size] = null;
+            size--;
             sink(1);
         }
-        list[i] = array[1];
+        list[i] = heap[1];
         return list;
     }
 
     public String toString() {
-        return Arrays.toString(array);
+        return Arrays.toString(heap);
     }
 
 
     public static void main(String[] args) {
         Random random = new Random(47);
-        int N = 10;
+        int N = 20;
         Integer[] a = new Integer[N];
         for (int i = 0; i < a.length; i++) {
             a[i] = random.nextInt(100);
         }
-        MinHeap<Integer> heap = new MinHeap<>(a);
+        MinPriorityQueue<Integer> heap = new MinPriorityQueue<>(a);
         System.out.println(heap);
         heap.insert(10);
-        heap.delete(1);
         heap.delete(5);
         heap.delMin();
         heap.delete(3);
         heap.delete(2);
-        heap.delMin();
         System.out.println(heap);
         System.out.println(Arrays.toString(heap.heapSort()));
     }
