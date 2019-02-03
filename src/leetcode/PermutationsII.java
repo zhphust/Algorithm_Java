d7375ba8582ca9edbf844976b3d2b5bffb6d0c60
zhphust <zhphust09@163.com>
2019-02-03 21:59:22 +0800

    47. 全排列 II（中等，回溯算法）

--- src/leetcode/PermutationsII.java
+++ src/leetcode/PermutationsII.java
@@ -5,49 +5,40 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
+ * 47. 全排列 II（中等，回溯算法）
+ * 给定一个可包含重复数字的序列，返回所有不重复的全排列。
  * @author Alisa.Wang zhphust09@hust.edu.cn at 2019/1/26
  * @version 8.0
  */
 public class PermutationsII {
     public List<List<Integer>> permuteUnique(int[] nums) {
-        List<List<Integer>> list=new ArrayList<>();
-        Arrays.sort(nums);
-
-        perm(list,nums, 0, nums.length - 1);
+        List<List<Integer>> list = new ArrayList<>();
+        if (nums.length == 0)
+            return list;
+        Arrays.sort(nums);                                 // 排序
+        boolean marked[] = new boolean[nums.length];       // 标记该元素是否被访问过
+        backTrace(list, new ArrayList<>(), nums, marked);  //（类似深度优先搜索）
         return list;
     }
 
-    private void perm(List<List<Integer>> list, int[] nums, int L, int R){
-        if(L==R){   //只有一位的时候，直接set
-            list.add(set(nums));
-        }else{
-            int i=L;
-            while (i <= R) {
-                int tmp=nums[i];
-                int j=i;
-                while(j<nums.length&&nums[j]==tmp){
-                    j++;
-                }
-                swap(nums,L, i);//将第i个数放到第一位固定，然后去排后面的，这样就不用使用hash表来保存使用了多少数字，以及哪些数字没有使用了
-                int[] a2=nums.clone();
-                perm(list,a2, L + 1, R);
-                i=j;
-
-            }
+    public void backTrace(List<List<Integer>> list, List<Integer> res, int nums[], boolean marked[]) {
+        if (res.size() == nums.length) {
+            list.add(new ArrayList<>(res));
+            return;
         }
-        return;
+        for (int i = 0; i < nums.length; i++) {
+            if(marked[i] || (i >= 1 && nums[i] == nums[i-1] && !marked[i-1]))
+                continue;
+            res.add(nums[i]);
+            marked[i] = true;
+            backTrace(list, res, nums, marked);
+            res.remove(res.size() - 1);
+            marked[i] = false;
+        }
     }
 
-    private List<Integer> set(int[] nums){
-        List<Integer> list=new ArrayList<>();
-        for(int i  =0; i < nums.length; i++){
-            list.add(nums[i]);
-        }
-        return list;
-    }
-    private void swap(int[] nums,int L,int R){
-        int tmp=nums[L];
-        nums[L]=nums[R];
-        nums[R]=tmp;
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 2};
+        System.out.println(new PermutationsII().permuteUnique(nums));
     }
 }
